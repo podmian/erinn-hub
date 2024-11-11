222bde4db989a7b2d79b1214d7d346ab79589993
podmian <popov.kontur@gmail.com>
2024-11-11 14:35:52 +0100

    Implement get/post user endpoints

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const uuid = require('uuid');
 
-app.get('/', (req, res) => {
-    res.status(200).send(`OK`);
+app.use(express.json());
+
+let userList = [];
+
+app.get('/users', (req, res) => {
+    res.status(200).send(userList);
+});
+
+app.get('/users/:id', (req, res) => {
+    userList.forEach((item) => {
+        if (item.id === req.params.id) {
+            res.status(200).send(item);
+        }
+    })
+    res.status(404).send('Not Found');
+
 })
+
+app.post('/users', (req, res) => {
+    const newUser = req.body
+    newUser.id = uuid.v6();
+    userList.push(newUser);
+    res.status(201).send(newUser.id);
+});
+
 
 app.listen(port, function() {console.log(`Listening on port ${port}`)});
